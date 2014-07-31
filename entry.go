acdee2204b7965b3679a03c98c1ab125a2ea279b
jonathanpittman <jonathanpittman@server.fake>
2014-07-30 22:58:47 -0400

    Changing Id in [tT]agId and [tT]ypeId to ID per golint.

--- entry.go
+++ entry.go
@@ -33,26 +33,26 @@ Entry structure
 // Entry represents a single entry in an IFD in a TIFF file.  This is the mostly
 // uninterpreted core 12 byte data structure only.
 type Entry interface {
-	TagId() uint16
-	TypeId() uint16
+	TagID() uint16
+	TypeID() uint16
 	Count() uint32
 	ValueOffset() [4]byte
 }
 
 // entry represents the data structure of an IFD entry.
 type entry struct {
-	tagId       uint16  // Bytes 0-1
-	typeId      uint16  // Bytes 2-3
+	tagID       uint16  // Bytes 0-1
+	typeID      uint16  // Bytes 2-3
 	count       uint32  // Bytes 4-7
 	valueOffset [4]byte // Bytes 8-11
 }
 
-func (e *entry) TagId() uint16 {
-	return e.tagId
+func (e *entry) TagID() uint16 {
+	return e.tagID
 }
 
-func (e *entry) TypeId() uint16 {
-	return e.typeId
+func (e *entry) TypeID() uint16 {
+	return e.typeID
 }
 
 func (e *entry) Count() uint32 {
@@ -64,18 +64,18 @@ func (e *entry) ValueOffset() [4]byte {
 }
 
 func (e *entry) String() string {
-	return fmt.Sprintf("<TagId: %5d, TypeId: %5d, Count: %d, ValueOffset: %v>", e.tagId, e.typeId, e.count, e.valueOffset)
+	return fmt.Sprintf("<TagID: %5d, TypeID: %5d, Count: %d, ValueOffset: %v>", e.tagID, e.typeID, e.count, e.valueOffset)
 }
 
 func (e *entry) MarshalJSON() ([]byte, error) {
 	tmp := struct {
-		Tag         uint16  `json:"tagId"`
-		Type        uint16  `json:"typeId"`
+		Tag         uint16  `json:"tagID"`
+		Type        uint16  `json:"typeID"`
 		Count       uint32  `json:"count"`
 		ValueOffset [4]byte `json:"valueOffset"`
 	}{
-		Tag:         e.tagId,
-		Type:        e.typeId,
+		Tag:         e.tagID,
+		Type:        e.typeID,
 		Count:       e.count,
 		ValueOffset: e.valueOffset,
 	}
@@ -84,10 +84,10 @@ func (e *entry) MarshalJSON() ([]byte, error) {
 
 func ParseEntry(br BReader) (out Entry, err error) {
 	e := new(entry)
-	if err = br.BRead(&e.tagId); err != nil {
+	if err = br.BRead(&e.tagID); err != nil {
 		return
 	}
-	if err = br.BRead(&e.typeId); err != nil {
+	if err = br.BRead(&e.typeID); err != nil {
 		return
 	}
 	if err = br.BRead(&e.count); err != nil {
@@ -102,26 +102,26 @@ func ParseEntry(br BReader) (out Entry, err error) {
 // Entry8 represents a single entry in an IFD8 in a BigTIFF file.  This is the
 // mostly uninterpreted core 20 byte data structure only.
 type Entry8 interface {
-	TagId() uint16
-	TypeId() uint16
+	TagID() uint16
+	TypeID() uint16
 	Count() uint64
 	ValueOffset() [8]byte
 }
 
 // entry8 represents the data structure of an IFD8 entry.
 type entry8 struct {
-	tagId       uint16  // Bytes 0-1
-	typeId      uint16  // Bytes 2-3
+	tagID       uint16  // Bytes 0-1
+	typeID      uint16  // Bytes 2-3
 	count       uint64  // Bytes 4-11
 	valueOffset [8]byte // Bytes 12-19
 }
 
-func (e8 *entry8) TagId() uint16 {
-	return e8.tagId
+func (e8 *entry8) TagID() uint16 {
+	return e8.tagID
 }
 
-func (e8 *entry8) TypeId() uint16 {
-	return e8.typeId
+func (e8 *entry8) TypeID() uint16 {
+	return e8.typeID
 }
 
 func (e8 *entry8) Count() uint64 {
@@ -133,18 +133,18 @@ func (e8 *entry8) ValueOffset() [8]byte {
 }
 
 func (e8 *entry8) String() string {
-	return fmt.Sprintf("<TagId: %5d, TypeId: %5d, Count: %d, ValueOffset: %v>", e8.tagId, e8.typeId, e8.count, e8.valueOffset)
+	return fmt.Sprintf("<TagID: %5d, TypeID: %5d, Count: %d, ValueOffset: %v>", e8.tagID, e8.typeID, e8.count, e8.valueOffset)
 }
 
 func (e8 *entry8) MarshalJSON() ([]byte, error) {
 	tmp := struct {
-		Tag         uint16  `json:"tagId"`
-		Type        uint16  `json:"typeId"`
+		Tag         uint16  `json:"tagID"`
+		Type        uint16  `json:"typeID"`
 		Count       uint64  `json:"count"`
 		ValueOffset [8]byte `json:"valueOffset"`
 	}{
-		Tag:         e8.tagId,
-		Type:        e8.typeId,
+		Tag:         e8.tagID,
+		Type:        e8.typeID,
 		Count:       e8.count,
 		ValueOffset: e8.valueOffset,
 	}
@@ -153,10 +153,10 @@ func (e8 *entry8) MarshalJSON() ([]byte, error) {
 
 func ParseEntry8(br BReader) (out Entry8, err error) {
 	e := new(entry8)
-	if err = br.BRead(&e.tagId); err != nil {
+	if err = br.BRead(&e.tagID); err != nil {
 		return
 	}
-	if err = br.BRead(&e.typeId); err != nil {
+	if err = br.BRead(&e.typeID); err != nil {
 		return
 	}
 	if err = br.BRead(&e.count); err != nil {
