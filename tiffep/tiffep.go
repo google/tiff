8419777c3f4d792d52ffa13a24ded3bcb9d35c6d
jonathanpittman <jonathanpittman@server.fake>
2014-07-31 23:12:54 -0400

    Adding a package for tiffep support.

---
+++ tiffep/tiffep.go
@@ -0,0 +1,20 @@
+package tiffep
+
+import "github.com/jonathanpittman/tiff"
+
+var tiffEPTags = tiff.NewTagSet("TIFF/EP", 32768, 65535)
+
+func init() {
+	tiffEPTags.Register(tiff.NewTag(33421, "CFARepeatPatternDim", nil))
+	tiffEPTags.Register(tiff.NewTag(33422, "CFAPattern", nil))
+	tiffEPTags.Register(tiff.NewTag(34859, "SelfTimeMode", nil))
+	tiffEPTags.Register(tiff.NewTag(37390, "FocalPlaneXResolution", nil))
+	tiffEPTags.Register(tiff.NewTag(37391, "FocalPlaneYResolution", nil))
+	tiffEPTags.Register(tiff.NewTag(37392, "FocalPlaneResolutionUnit", nil))
+	tiffEPTags.Register(tiff.NewTag(37398, "TIFF/EPStandardID", nil))
+	tiffEPTags.Register(tiff.NewTag(37399, "SensingMethod", nil))
+
+	tiffEPTags.Lock()
+
+	tiff.DefaultTagSpace.RegisterTagSet(tiffEPTags)
+}
