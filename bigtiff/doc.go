5c5677d2dfac34ae94243a972b318a3604570f8c
jonathanpittman <jonathanpittman@server.fake>
2014-07-31 23:26:28 -0400

    Fixing up package comments in doc.go files.

---
+++ bigtiff/doc.go
@@ -0,0 +1,8 @@
+/*
+Package bigtiff implements structures and functionality for working with BigTIFF data structures.
+
+References:
+  [BIGTIFF]:       http://www.awaresystems.be/imaging/tiff/bigtiff.html
+  [BIGTIFFDESIGN]: http://www.remotesensing.org/libtiff/bigtiffdesign.html
+*/
+package bigtiff
--- doc.go
+++ doc.go
@@ -1,16 +1,9 @@
 /*
-Package tiff implements structures and functionality for working with TIFF and BigTIFF files.
+Package tiff implements structures and functionality for working with TIFF data structures.
 
 References:
   [TIFF6]:         http://partners.adobe.com/public/developer/en/tiff/TIFF6.pdf
   [TIFFPM6]:       http://partners.adobe.com/public/developer/en/tiff/TIFFPM6.pdf
   [TIFFTAGS]:      http://www.digitalpreservation.gov/formats/content/tiff_tags.shtml
-  [GEOTIFF]:       http://www.remotesensing.org/geotiff/spec/geotiffhome.html
-  [BIGTIFF]:       http://www.awaresystems.be/imaging/tiff/bigtiff.html
-  [BIGTIFFDESIGN]: http://www.remotesensing.org/libtiff/bigtiffdesign.html
-
-TODO:
-  Implement the basic representation func for each default field type.
-  Add valid field types to all of the tags.
 */
 package tiff
