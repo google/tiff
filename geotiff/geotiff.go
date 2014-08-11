e2f6f1bc763821a103a33ead763d41930e3cd015
jonathanpittman <jonathanpittman@server.fake>
2014-08-11 17:37:21 -0300

    Adding tags for geotiff

---
+++ geotiff/geotiff.go
@@ -0,0 +1,19 @@
+package geotiff
+
+import "github.com/jonathanpittman/tiff"
+
+var geotiffTags = tiff.NewTagSet("GeoTIFF", 32768, 65535)
+
+func init() {
+	geotiffTags.Register(tiff.NewTag(33550, "ModelPixelScaleTag", nil))
+	geotiffTags.Register(tiff.NewTag(34264, "ModelTransformationTag", nil))
+	geotiffTags.Register(tiff.NewTag(33922, "ModelTiepointTag", nil))
+	geotiffTags.Register(tiff.NewTag(34735, "GeoKeyDirectoryTag", nil))
+	geotiffTags.Register(tiff.NewTag(34736, "GeoDoubleParamsTag", nil))
+	geotiffTags.Register(tiff.NewTag(34737, "GeoAsciiParamsTag", nil))
+	geotiffTags.Register(tiff.NewTag(33920, "IntergraphIrasBMatrixTag", nil))
+
+	geotiffTags.Lock()
+
+	tiff.DefaultTagSpace.RegisterTagSet(geotiffTags)
+}
