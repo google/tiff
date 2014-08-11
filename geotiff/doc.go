ac612403299343df9950ff299358b43cde4066bf
jonathanpittman <jonathanpittman@server.fake>
2014-08-11 17:30:59 -0300

    Adding URL references for geotiff

--- geotiff/doc.go
+++ geotiff/doc.go
@@ -1,5 +1,6 @@
 /*
 References:
-  [GEOTIFF]:       http://www.remotesensing.org/geotiff/spec/geotiffhome.html
+  [GEOTIFF]:    http://www.remotesensing.org/geotiff/spec/geotiffhome.html
+  [INTERGRAPH]: http://www.awaresystems.be/imaging/tiff/tifftags/docs/intergraph.html
 */
 package geotiff
