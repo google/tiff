a9bf316af6f242058497292cb4cb97ccdd948ae8
jonathanpittman <jonathanpittman@server.fake>
2014-08-11 17:39:40 -0300

    Adding modi package for Microsoft Office Document Imaging tag support

---
+++ modi/modi.go
@@ -0,0 +1,17 @@
+// Package modi provides tiff extensions for working with Microsoft Office
+// Document Imaging based tiff files.
+package modi
+
+import "github.com/jonathanpittman/tiff"
+
+var modiTags = tiff.NewTagSet("MODI", 32768, 65535)
+
+func init() {
+	modiTags.Register(tiff.NewTag(37679, "MODIText", nil))
+	modiTags.Register(tiff.NewTag(37680, "MODIOLEPropertySetStorage", nil))
+	modiTags.Register(tiff.NewTag(37681, "MODIPositioning", nil))
+
+	modiTags.Lock()
+
+	tiff.DefaultTagSpace.RegisterTagSet(modiTags)
+}
