037c55654aaf23605b4772321ef86e17ae7dfa90
jonathanpittman <jonathanpittman@server.fake>
2015-02-21 14:41:35 -0500

    Change the method ValidFieldTypes to Interpreter in the Tag interface
    ValidFieldTypes() was not being used and its use was not going to be
    simple.  Sporting an Interpreter() method that returns a
    FieldInterpreter is simpler and more useful.

--- tag.go
+++ tag.go
@@ -7,17 +7,18 @@ import (
 type Tag interface {
 	ID() uint16
 	Name() string
-	ValidFieldTypes() []FieldType
+	Interpreter() FieldInterpreter
+	//ValidFieldTypes() []FieldType
 }
 
-func NewTag(id uint16, name string, validFTs []FieldType) Tag {
-	return &tag{id: id, name: name, validFTs: validFTs}
+func NewTag(id uint16, name string, fi FieldInterpreter) Tag {
+	return &tag{id: id, name: name, fi: fi}
 }
 
 type tag struct {
-	id       uint16
-	name     string
-	validFTs []FieldType
+	id   uint16
+	name string
+	fi   FieldInterpreter
 }
 
 func (t *tag) ID() uint16 {
@@ -31,6 +32,25 @@ func (t *tag) Name() string {
 	return fmt.Sprintf("UNNAMED_TAG_%d", t.id)
 }
 
-func (t *tag) ValidFieldTypes() []FieldType {
-	return t.validFTs
+func (t *tag) Interpreter() FieldInterpreter {
+	if t.fi == nil {
+		return defaultFieldInterpreter
+	}
+	return t.fi
 }
+
+type FieldInterpreter func(Field) string
+
+func defaultFieldInterpreter(f Field) string {
+	return ""
+}
+
+/*
+type FieldInterpreter interface {
+	Interpret(Field) string
+	Register(reg interface{}) bool
+	// Reg, in this case, could be anything, but usually a struct or map with a key/value pair.
+	// The idea is that the FieldInterpreter should know what to do with the thing or return false.
+	//Register(key, val reflect.Value) bool
+}
+*/
